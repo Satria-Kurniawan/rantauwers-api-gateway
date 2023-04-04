67801949187f872c26a1f745860008fb3679a9b0
Satria-Kurniawan <satriakurniawan2001@gmail.com>
2023-04-04 15:24:01 +0800

    update

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.send("API Gateway is ready!");
+});
+
 app.use(
   "/api-gateway",
   upload.fields([
